8fc7b786f0340dfdea28013c84fcfd0d6e282e09
nmhyrhn <gbs06193@gmail.com>
2025-11-21 09:54:24 +0900

    [Bronze IV] Title: R2, Time: 164 ms, Memory: 17516 KB -BaekjoonHub

---
+++ Java/백준/Bronze/3046. R2/R2.java
@@ -0,0 +1,13 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+   public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        int r1 = sc.nextInt();
+        int s = sc.nextInt();
+        System.out.println(2*s - r1);
+    }
+}
