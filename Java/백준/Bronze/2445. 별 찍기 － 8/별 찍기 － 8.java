e55e7fdc061901731da782ce73eabb474432471c
nmhyrhn <gbs06193@gmail.com>
2025-12-18 09:31:31 +0900

    [Bronze III] Title: 별 찍기 - 8, Time: 116 ms, Memory: 14404 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2445. 별 찍기 － 8/별 찍기 － 8.java
@@ -0,0 +1,41 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+     public static void main(String[] args) throws IOException {
+       
+         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        
+         int n = Integer.parseInt(br.readLine());
+        StringBuilder sb = new StringBuilder();
+         
+        for (int i = 1; i <= n; i++) {
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            for (int j = 1; j <= 2*n-2*i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+         
+        for (int i = n-1; i >= 1; i--) {
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            for (int j = 1; j <= 2*n-2*i; j++) {
+                sb.append(" ");
+            }
+            for (int j = 1; j <= i; j++) {
+                sb.append("*");
+            }
+            sb.append("\n");
+        }
+        System.out.print(sb);
+    }
+}
