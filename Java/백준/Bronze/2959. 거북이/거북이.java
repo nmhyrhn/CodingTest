74648189421e23322ef567d6257bb72fc9627307
nmhyrhn <gbs06193@gmail.com>
2026-01-09 19:16:01 +0900

    [Bronze III] Title: 거북이, Time: 104 ms, Memory: 14116 KB -BaekjoonHub

---
+++ Java/백준/Bronze/2959. 거북이/거북이.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[4];
+        for (int i = 0; i < 4; i++) arr[i] = Integer.parseInt(st.nextToken());
+
+        Arrays.sort(arr);
+        int ans = arr[0] * arr[2]; 
+        System.out.println(ans);
+    }
+}
