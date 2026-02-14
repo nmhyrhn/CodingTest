040d9eb5ceb05dc9936e3c8c9d274a8e01256e94
nmhyrhn <gbs06193@gmail.com>
2026-02-14 14:32:32 +0900

    [Unrated] Title: 더하기, Time: 164 ms, Memory: 17580 KB -BaekjoonHub

---
+++ Java/백준/Unrated/14918. 더하기/더하기.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) {
+
+    Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+    
+        System.out.println((a+b));
+    }
+}
