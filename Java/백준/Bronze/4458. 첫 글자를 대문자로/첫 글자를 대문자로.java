0bacce028d965dd0449214c05d405410c8486540
nmhyrhn <gbs06193@gmail.com>
2026-02-17 17:17:28 +0900

    [Bronze III] Title: 첫 글자를 대문자로, Time: 112 ms, Memory: 16000 KB -BaekjoonHub

---
+++ Java/백준/Bronze/4458. 첫 글자를 대문자로/첫 글자를 대문자로.java
@@ -0,0 +1,22 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+
+        for (int i = 0; i < n; i++) {
+            String s = br.readLine();
+
+            char first = Character.toUpperCase(s.charAt(0));
+
+            String result = first + s.substring(1);
+
+            System.out.println(result);
+        }
+    }
+}
