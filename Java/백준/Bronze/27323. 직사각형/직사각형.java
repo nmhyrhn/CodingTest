9044b8f30776805dfad6d1a64c860efd81405276
nmhyrhn <gbs06193@gmail.com>
2026-03-30 19:08:31 +0900

    [Bronze V] Title: 직사각형, Time: 96 ms, Memory: 14264 KB -BaekjoonHub

---
+++ Java/백준/Bronze/27323. 직사각형/직사각형.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+// The main method must be in a class named "Main".
+class Main {
+    public static void main(String[] args) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        
+        System.out.println(n*m);
+    }
+}
